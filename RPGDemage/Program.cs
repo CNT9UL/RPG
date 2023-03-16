610a1c79b096af816a53284404550391082343a1
SmokerG <sensei@stud.uni-obuda.hu>
2023-03-16 12:25:55 +0100

    1.0 glhf

--- RPGDemage/Program.cs
+++ RPGDemage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace RPGDemage
 {
@@ -6,7 +7,43 @@ namespace RPGDemage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Start:
+            Console.WriteLine("Hello World Hero!");
+            Console.WriteLine("Press any key (except the power button) to know how many damage did your Hero taken and dealt!");
+            Console.ReadLine();
+
+            int damageReceived = 0;
+            int damageDealt = 0;
+            int roundCount = 0;
+
+            using (StreamReader sr = new StreamReader("attack_log.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(' ');
+                    if (parts.Length == 2)
+                    {
+                        if (parts[0] == "get")
+                        {
+                            damageReceived += Int32.Parse(parts[1]);
+                        }
+                        else if (parts[0] == "give")
+                        {
+                            damageDealt += Int32.Parse(parts[1]);
+                        }
+                        roundCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Total damage received: " + damageReceived);
+            Console.WriteLine("Total damage dealt: " + damageDealt);
+            Console.WriteLine($"In {roundCount} rounds");
+            Console.WriteLine("Press any key (except the power button) to continue!");
+            Console.ReadLine();
+            Console.Clear();
+            goto Start;
         }
     }
 }
